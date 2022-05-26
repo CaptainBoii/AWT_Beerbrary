35e70d3f27246555bd6f7104e6ca3c00c5d93c66
CaptainBoii <77623616+CaptainBoii@users.noreply.github.com>
2022-05-26 20:18:26 +0200

    Registration fix

--- beerbrary/files/views.py
+++ beerbrary/files/views.py
@@ -171,32 +171,34 @@ def get_beers(request):
 
 def register_user(request):
     response_data = {}
+    print("not here1")
     request_data = decode(request)
     username = request_data['username']
     password = request_data['password']
     login = request_data['login']
-    password_duplicate = False
+    print("not here2")
     name_duplicate = False
     login_duplicate = False
     for user in User.objects.raw('SELECT * FROM files_user'):
         if user.name == username:
             name_duplicate = True
-        if user.password == password:
-            password_duplicate = True
         if user.login == login:
             login_duplicate = True
-    if not password_duplicate and not login_duplicate and not name_duplicate:
+    print("not here3")
+    if not login_duplicate and not name_duplicate:
         user = User()
         user.name = username
         user.login = login
         user.password = password
-        user.theme_id = PrefferedThemes.objects.get(id=1)
+        user.theme_id = PrefferedThemes.objects.get(id=1).id
+        print("not here4")
         user.save()
+        print("not here5")
         return JsonResponse({"status": "ok"})
     else:
+        print("not here6")
         response_data.__setitem__("login", login_duplicate)
         response_data.__setitem__("username", name_duplicate)
-        response_data.__setitem__("password", password_duplicate)
         return HttpResponse(json.dumps(response_data))
 
 def login(request):
